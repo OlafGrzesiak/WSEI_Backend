8be3ac796641a92c0e783689dfffee17f35044fa
olaf.grzesiak <olaf.grzesiak@microsoft.wsei.edu.pl>
2024-03-20 14:04:52 +0100

    Initial commit

---
+++ Web/SeedData.cs
@@ -0,0 +1,24 @@
+﻿using ApplicationCore.Interfaces.Repository;
+using BackendLab01;
+
+namespace Infrastructure.Memory;
+public static class SeedData
+{
+    public static void Seed(this WebApplication app)
+    {
+        using (var scope = app.Services.CreateScope())
+        {
+            var provider = scope.ServiceProvider;
+            var quizRepo = provider.GetService<IGenericRepository<Quiz, int>>();
+            var quizItemRepo = provider.GetService<IGenericRepository<QuizItem, int>>();
+
+            var q1 = new QuizItem(101, "3 + 5", new List<string>() { "2", "4", "6" }, "8");
+            var q2 = new QuizItem(101, "3 + 5", new List<string>() { "2", "4", "6" }, "8");
+
+            quizItemRepo.Add(q1);
+            quizItemRepo.Add(q2);
+            var quiz = new Quiz(1, new List<QuizItem>() { q1, q2 }, "Arytmetyka");
+            quizRepo.Add(quiz);
+        }
+    }
+}
---
+++ WebAPI/Controllers/ApiQuizUserController.cs
@@ -0,0 +1,23 @@
+﻿using BackendLab01;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers;
+
+[ApiController]
+[Route("/api/v1/users/quizzes")]
+public class ApiQuizUserController : ControllerBase
+{
+    private IQuizUserService _service;
+
+    public ApiQuizUserController(IQuizUserService service)
+    {
+        _service = service;
+    }
+
+    [HttpGet]
+    [Route("{id}")]
+    public Quiz? FindQuizById(int id)
+    {
+        return _service.FindQuizById(id);
+    }
+}
